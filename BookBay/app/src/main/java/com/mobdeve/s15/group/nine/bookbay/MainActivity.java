bc6d355df789eff8cf0f32fb60f83eec9ff160f3
Ivy Tan <ivy_tan_b@dlsu.edu.ph>
2021-08-14 15:49:55 +0800

    added home page

--- BookBay/app/src/main/java/com/mobdeve/s15/group/nine/bookbay/MainActivity.java
+++ BookBay/app/src/main/java/com/mobdeve/s15/group/nine/bookbay/MainActivity.java
@@ -4,11 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        // Configure sign-in to request the user's ID, email address, and basic
+//// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                .requestEmail()
+//                .build();
+//
+//        // Build a GoogleSignInClient with the options specified by gso.
+//        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+//        // Check for existing Google Sign In account, if the user is already signed in
+//// the GoogleSignInAccount will be non-null.
+//        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+//        updateUI(account);
     }
 }
