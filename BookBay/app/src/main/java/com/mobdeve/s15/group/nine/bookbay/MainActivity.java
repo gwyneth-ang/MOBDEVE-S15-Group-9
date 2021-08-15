d1b88e81660ddcf3fa67e11a783972a23be94100
Gwyneth Ang <gwyneth_d_ang@dlsu.edu.ph>
2021-08-15 22:20:36 +0800

    added layouts for view account, view booking details and view book details

--- BookBay/app/src/main/java/com/mobdeve/s15/group/nine/bookbay/MainActivity.java
+++ BookBay/app/src/main/java/com/mobdeve/s15/group/nine/bookbay/MainActivity.java
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+//import com.google.android.gms.auth.api.signin.GoogleSignIn;
+//import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+//import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class MainActivity extends AppCompatActivity {
 
