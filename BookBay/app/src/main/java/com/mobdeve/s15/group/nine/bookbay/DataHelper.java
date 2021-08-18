0b2e1d2d858d84dc9c1a1fbf5ca24d2aca2958ad
chrissyv09 <christianne_marie_vinco@dlsu.edu.ph>
2021-08-18 07:49:28 -0700

    done with notifications

--- BookBay/app/src/main/java/com/mobdeve/s15/group/nine/bookbay/DataHelper.java
+++ BookBay/app/src/main/java/com/mobdeve/s15/group/nine/bookbay/DataHelper.java
@@ -2,6 +2,9 @@ package com.mobdeve.s15.group.nine.bookbay;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class DataHelper {
 
@@ -21,18 +24,27 @@ public class DataHelper {
     }
 
     public static ArrayList<Notification> populateNotifications() {
+        Date date = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
         ArrayList<Notification> notifications = new ArrayList<>();
         String[] titles = {"Girl, Woman, Other", "Shoko’s Smile", "The War for Kindness: Building Empathy in a Fractured World", "The Death of Vivek Oji", "The Little Prince"};
         String[] sellers = {"Lea Salonga", "Namjoon Kim", "Korina Sanchez", "Justin Bieber", "Taylor Swift"};
         String[] status = {"Approved", "Approved", "Declined", "Approved", "Declined"};
         int[] pictures = {R.drawable.girl, R.drawable.shoko_smile, R.drawable.war, R.drawable.death, R.drawable.little_prince};
 
+        int time = 3;
+
         for (int i=0; i<titles.length;i++){
+            time += 2;
+            calendar.set(Calendar.HOUR_OF_DAY, time);
             notifications.add(new Notification(
                     titles[i],
                     sellers[i],
                     status[i],
-                    pictures[i]));
+                    pictures[i],
+                    calendar));
         }
         return notifications;
     }
--- BookBay/app/src/main/java/com/mobdeve/s15/group/nine/bookbay/Notification.java
+++ BookBay/app/src/main/java/com/mobdeve/s15/group/nine/bookbay/Notification.java
@@ -1,21 +1,57 @@
 package com.mobdeve.s15.group.nine.bookbay;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class Notification {
     private String bookTitle;
     private String sellerName;
     private String status;
-    private String time;
     private int imageId;
+    private Calendar timeMade;
+    private long toMilli;
 
-    public Notification(String bookTitle, String sellerName, String status, int imageId) {
+    private static final int SECOND_MILLIS = 1000;
+    private static final int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+    private static final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+    private static final int DAY_MILLIS = 24 * HOUR_MILLIS;
+
+    public Notification(String bookTitle, String sellerName, String status, int imageId, Calendar timeMade) {
         this.bookTitle = bookTitle;
         this.sellerName = sellerName;
         this.status = status;
-        this.time = "5 MINS AGO";
         this.imageId = imageId;
+        this.timeMade = timeMade;
     }
+
+    /*public static String getTimeAgo(long time) {
+        if (time < 1000000000000L) {
+            time *= 1000;
+        }
+
+        long now = System.currentTimeMillis();
+        if (time > now || time <= 0) {
+            return null;
+        }
+
+
+        final long diff = now - time;
+        if (diff < MINUTE_MILLIS) {
+            return "just now";
+        } else if (diff < 2 * MINUTE_MILLIS) {
+            return "a minute ago";
+        } else if (diff < 50 * MINUTE_MILLIS) {
+            return diff / MINUTE_MILLIS + " minutes ago";
+        } else if (diff < 90 * MINUTE_MILLIS) {
+            return "an hour ago";
+        } else if (diff < 24 * HOUR_MILLIS) {
+            return diff / HOUR_MILLIS + " hours ago";
+        } else if (diff < 48 * HOUR_MILLIS) {
+            return "yesterday";
+        } else {
+            return diff / DAY_MILLIS + " days ago";
+        }
+    }*/
 
     public String getBookTitle() { return bookTitle; }
 
@@ -23,9 +59,7 @@ public class Notification {
 
     public String getStatus() { return status; }
 
-    public String getTimePosted() { return time; }
-
     public int getImageId() { return imageId; }
 
-    public String getTime() { return time; }
+    public String getTime() { return timeMade.getTime().toString(); }
 }
