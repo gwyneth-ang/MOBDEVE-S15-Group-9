4744f5d4aec86c31b35e83553a44e4eab0930dd5
chrissyv09 <christianne_marie_vinco@dlsu.edu.ph>
2021-08-17 21:36:52 -0700

    added something

--- BookBay/app/src/main/java/com/mobdeve/s15/group/nine/bookbay/NotificationsAdapter.java
+++ BookBay/app/src/main/java/com/mobdeve/s15/group/nine/bookbay/NotificationsAdapter.java
@@ -1,6 +1,7 @@
 package com.mobdeve.s15.group.nine.bookbay;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
@@ -8,17 +9,28 @@ import android.view.View;
 
 import java.util.ArrayList;
 
-public class NotificationsAdapter extends AppCompatActivity {
+public class NotificationsAdapter extends RecyclerView.Adapter<NotificationsViewHolder> {
 
     private ArrayList<Notification> notifications;
 
     public NotificationsAdapter(ArrayList<Notification> data) {this.notifications = data;}
 
+    @Override
     public NotificationsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View itemView = layoutInflater.inflate(R.layout.notifications_layout, parent, false);
         NotificationsViewHolder holder = new NotificationsViewHolder(itemView);
 
         return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(NotificationsViewHolder holder, int position) {
+        holder.bindData(notifications.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return notifications.size();
     }
 }
