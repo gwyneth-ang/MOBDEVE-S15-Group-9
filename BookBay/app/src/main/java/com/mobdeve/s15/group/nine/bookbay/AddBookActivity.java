0318acd6b32a3d1d996abea33cb3a77d9d89cf14
chrissyv09 <christianne_marie_vinco@dlsu.edu.ph>
2021-08-18 17:08:11 -0700

    pushing

---
+++ BookBay/app/src/main/java/com/mobdeve/s15/group/nine/bookbay/AddBookActivity.java
@@ -0,0 +1,15 @@
+package com.mobdeve.s15.group.nine.bookbay;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddBookActivity extends AppCompatActivity {
+
+    @Override
+    protected  void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_and_edit_book);
+    }
+
+}
