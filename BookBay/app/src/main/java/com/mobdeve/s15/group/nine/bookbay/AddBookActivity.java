2ec7b12571bcc07e4583335dda781af64cce326f
Ivy Tan <ivy_tan_b@dlsu.edu.ph>
2021-09-07 13:19:56 +0800

    tried camera

--- BookBay/app/src/main/java/com/mobdeve/s15/group/nine/bookbay/AddBookActivity.java
+++ BookBay/app/src/main/java/com/mobdeve/s15/group/nine/bookbay/AddBookActivity.java
@@ -2,9 +2,14 @@ package com.mobdeve.s15.group.nine.bookbay;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -20,6 +25,7 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -27,12 +33,19 @@ import com.mobdeve.s15.group.nine.bookbay.model.BookbayFirestoreHelper;
 import com.mobdeve.s15.group.nine.bookbay.model.BookbayFirestoreReferences;
 import com.mobdeve.s15.group.nine.bookbay.model.Books_sell;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Callback;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Calendar;
 import java.util.TimeZone;
+import java.util.Date;
+
+import java.text.SimpleDateFormat;
 
 public class AddBookActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     public static String
@@ -54,25 +67,76 @@ public class AddBookActivity extends AppCompatActivity implements AdapterView.On
     private int ViewKey = 0, spinnerIndex;
     private boolean imageChanged = false;
 
-    private ActivityResultLauncher<Intent> myActivityResultLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode() == Activity.RESULT_OK){
-                        try {
-                            if(result.getData() != null) {
-                                // Get the path of the image
-                                imageUri = result.getData().getData();
-                                // Load the image into the tempImageIv using the path
-                                Picasso.get().load(imageUri).into(Iv_bookImage);
-                            }
-                        } catch(Exception exception){
-                            Log.d("TAG",""+exception.getLocalizedMessage());
-                        }
-                    }
+    private String currentPhotoPath;
+
+//    private ActivityResultLauncher<Intent> myActivityResultLauncher = registerForActivityResult(
+//            new ActivityResultContracts.StartActivityForResult(),
+//            new ActivityResultCallback<ActivityResult>() {
+//                @Override
+//                public void onActivityResult(ActivityResult result) {
+//                    if (result.getResultCode() == Activity.RESULT_OK){
+//                        try {
+//                            if(result.getData() != null) {
+//                                // Get the path of the image
+//                                imageUri = result.getData().getData();
+//                                // Load the image into the tempImageIv using the path
+//                                Picasso.get().load(imageUri).into(Iv_bookImage);
+//                            }
+//                        } catch(Exception exception){
+//                            Log.d("TAG",""+exception.getLocalizedMessage());
+//                        }
+//                    }
+//                }
+//            });
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch(requestCode) {
+            case 0:
+                if(resultCode == RESULT_OK){
+                    imageUri = imageReturnedIntent.getData();
+                    Picasso.get().load(imageUri).into(Iv_bookImage);
+
+                    Log.d("HERE IN CHOOSE", imageUri.toString());
                 }
-            });
+
+                break;
+            case 1:
+                if(resultCode == RESULT_OK){
+
+//                    imageUri = imageReturnedIntent.getData();
+//                    imageUri = Uri.parse(imageReturnedIntent.getData().getStringExtra(MediaStore.EXTRA_OUTPUT));
+//                    imageUri = imageReturnedIntent.getData();
+//                    galleryAddPic();
+
+//                    Bundle extras = imageReturnedIntent.getExtras();
+//
+//                    imageUri = (Uri) extras.get("data");
+//
+                    // ANOTHER VERSION
+//                    Log.d("HERE TOO", imageUri.toString());
+//                    // FIXME: does not work yung gallery
+//                    galleryAddPic();
+//
+//                    // PISCASOO DOES NOT WORK
+//                    Picasso.get().load(imageUri).into(Iv_bookImage, new Callback() {
+//                        @Override
+//                        public void onSuccess() {
+//                        }
+//
+//                        @Override
+//                        public void onError(Exception e) {
+//                            e.printStackTrace();
+//                        }
+//                    });
+//
+//                    Bundle extras = imageReturnedIntent.getExtras();
+//                    Bitmap bmp = (Bitmap) extras.get("data");
+//                    Iv_bookImage.setImageBitmap(bmp);
+                }
+                break;
+        }
+    }
 
     @Override
     protected  void onCreate(Bundle savedInstanceState) {
@@ -143,12 +207,65 @@ public class AddBookActivity extends AppCompatActivity implements AdapterView.On
         this.Bt_browse_addBook.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                String[] choice = {"Take Photo", "Choose From Gallery"};
                 imageChanged = true;
 
-                Intent i = new Intent();
-                i.setType("image/*");
-                i.setAction(Intent.ACTION_OPEN_DOCUMENT);
-                myActivityResultLauncher.launch(Intent.createChooser(i, "Select Picture"));
+                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                builder.setTitle("Add Photo");
+                builder.setItems(choice, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (choice[which].equals("Take Photo")) {
+                            Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+//                            File file=getOutputMediaFile(1);
+//                            picUri = Uri.fromFile(file); // create
+//                            i.putExtra(MediaStore.EXTRA_OUTPUT,picUri); // set the image file
+
+//                            if (takePicture.resolveActivity(getPackageManager()) != null) {
+//                                startActivityForResult(takePicture, 1);
+//                            }
+
+                            if (takePicture.resolveActivity(getPackageManager()) != null) {
+                                // Create the File where the photo should go
+                                File photoFile = null;
+                                try {
+                                    photoFile = createImageFile();
+                                } catch (IOException ex) {
+                                    // Error occurred while creating the File
+                                    ex.printStackTrace();
+                                }
+                                // Continue only if the File was successfully created
+                                if (photoFile != null) {
+                                    Uri photoUri = FileProvider.getUriForFile(view.getContext(),
+                                            "com.example.android.fileprovider",
+                                            photoFile);
+
+                                    Log.d("HERE", photoUri.toString());
+                                    takePicture.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                                    startActivityForResult(takePicture, 1);
+                                }
+                            }
+                        } else if (choice[which].equals("Choose From Gallery")) {
+                            Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            pickPhoto.setType("image/*");
+
+                            startActivityForResult(pickPhoto, 0);
+                        }
+                    }
+                });
+                builder.show();
+
+
+
+//                imageChanged = true;
+//
+//                Intent i = new Intent();
+//                i.setType("image/*");
+//                i.setAction(Intent.ACTION_OPEN_DOCUMENT);
+//                myActivityResultLauncher.launch(Intent.createChooser(i, "Select Picture"));
             }
         });
 
@@ -289,4 +406,47 @@ public class AddBookActivity extends AppCompatActivity implements AdapterView.On
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, imageUri);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        Log.d("HERE GALLERY", contentUri.toString());
+        sendBroadcast(mediaScanIntent);
+    }
+
+//    @Override
+//    protected void onSaveInstanceState(Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//
+//        outState.putParcelable("imageUri", imageUri);
+//    }
+//
+//    // Recover the saved state when the activity is recreated.
+//    @Override
+//    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+//        super.onRestoreInstanceState(savedInstanceState);
+//
+//        imageUri = savedInstanceState.getParcelable("imageUri");
+//
+//    }
+
+
 }
